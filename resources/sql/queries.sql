4a4e9a9eeec47a0a8eac208052885c45882a4380
Tom <mctighe.tom@gmail.com>
2019-07-25 17:28:07 -0700

    fixed country

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,12 +1,12 @@
 -- :name get-events :? :*
 -- :doc selects all events given user-selected filters, I imagine this is really slow for big lists
-SELECT DISTINCT events.name, events.id, month, dates, full_date
+SELECT DISTINCT events.name, events.id, month, dates, date
 FROM events, books, event_to_books
 WHERE events.id=events.id
-   --~ (if (seq (:region params)) " AND region IN (:v*:region)")
+   --~ (if (seq (:region params)) " AND country IN (:v*:region)")
    --~ (if (seq (:month params)) " AND month IN (:v*:month)")
    --~ (if (seq (:book params)) " AND events.id = event_to_books.event_id AND event_to_books.book_id = books.id AND book_name IN (:v*:book)")
-ORDER BY full_date
+ORDER BY date
 
 -- :name get-regions :? :*
 -- :doc selects regions
